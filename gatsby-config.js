5e71ab97236db94cbc3f5c201a07b4d5da09764c
Tigran Astvatsatryan <tigran.terian@gmail.com>
2019-04-28 13:04:23 +0400

    cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    title: 'TIL.ACADEMY',
+    author: '',
+    description: 'Weekly updates on cool tech in a Today I Learned fashion'
   },
   plugins: [
     {
